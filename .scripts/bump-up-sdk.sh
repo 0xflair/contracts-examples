d3c88f24c5bd89fc07ce1655e9783818dcebd3fb
aramalipoor <aram.alipoor@gmail.com>
2022-10-07 16:16:16 +0200

    chore: dump up sdk to latest

---
+++ .scripts/bump-up-sdk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd ${SCRIPT_DIR}/../next.js/custom-tiered-sales
+yarn remove @flair-sdk/react && yarn add @flair-sdk/react@latest
+
+cd ${SCRIPT_DIR}/../next.js/simple-wallet-integration
+yarn remove @flair-sdk/react && yarn add @flair-sdk/react@latest
+
+
+cd ${SCRIPT_DIR}/../react/custom-tiered-sales
+npm remove @flair-sdk/react && npm install @flair-sdk/react@latest
+
+cd ${SCRIPT_DIR}/../react/simple-wallet-integration
+npm remove @flair-sdk/react && npm install @flair-sdk/react@latest
